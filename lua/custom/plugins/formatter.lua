9768a0da02dc60247b6a1d0d6c0f36b5fe4a6b26
Amin Mir <aminmir326@gmail.com>
2023-12-08 13:14:33 +0200

    Add formatter for js files

---
+++ lua/custom/plugins/formatter.lua
@@ -0,0 +1,31 @@
+return {
+  'mhartington/formatter.nvim',
+  config = function()
+    local formatter = require("formatter")
+    formatter.setup {
+      ft = {
+        js = {
+          { exe = "prettier",      args = { "--stdin-filepath", "%filepath" }, },
+          { exe = "eslint-format", args = { "--stdin" }, },
+        },
+        lua = {
+          require('formatter.filetypes.lua').stylua
+        }
+      },
+    }
+
+    local autocmd = vim.api.nvim_create_autocmd
+
+    autocmd("BufWritePost", {
+      pattern = "*.js",
+      callback = function()
+        vim.cmd("Format")
+      end,
+    })
+
+    -- vim.api.nvim_create_autocmd("BufWritePost", {
+    --  pattern = "*",
+    --  command = "lua vim.lsp.buf.formatting()",
+    -- })
+  end,
+}

c5b73b6ae2a6e5f987e3ec2e2e4a901e10f70de7
Amin Mir <aminmir326@gmail.com>
2023-12-08 10:29:15 +0200

    Fix indentations

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,19 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	version = "*",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require('neo-tree').setup {
-			window = {
-				mappings = {
-					["l"] = "open",
-					["h"] = "close_node",
-				},
-			},
-		}
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        -- follow_current_file = true,
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        }
+      },
+      window = {
+        position = "float",
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+        },
+      },
+    }
+  end,
 }

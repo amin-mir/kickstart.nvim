0094b306655070e3b2f09546ce7c84a196a58a9b
Amin Mir <aminmir326@gmail.com>
2023-12-08 10:29:49 +0200

    Update filetree keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,8 @@
 -- NeoTree shortcuts
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree position=left toggle<CR>', { desc = 'Open Neotree in floating mode' })
-vim.keymap.set('n', '<leader>ef', '<Cmd>Neotree position=float reveal toggle<CR>',
+vim.keymap.set('n', '<leader>tf', '<Cmd>Neotree position=float reveal toggle<CR>',
   { desc = 'Open Neotree in floating mode' })
-vim.keymap.set('n', '<leader>eb', '<Cmd>Neotree source=buffers position=float toggle<CR>', { desc = 'Show open buffers' })
+vim.keymap.set('n', '<leader>tb', '<Cmd>Neotree source=buffers position=float toggle<CR>', { desc = 'Show open buffers' })
+
+-- Git
+vim.keymap.set('n', '<leader>gb', '<Cmd>:GBrows<CR>', { desc = 'Brows to the current file on the git platform' })

8c199e7fd7695fd41c312dfbd9a3b0ccec2ce2d2
Amin Mir <aminmir326@gmail.com>
2023-12-08 13:13:56 +0200

    Add file tree to which-key

--- init.lua
+++ init.lua
@@ -566,6 +566,7 @@ end
 require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+  ['<leader>f'] = { name = '[F]ile Tree', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },

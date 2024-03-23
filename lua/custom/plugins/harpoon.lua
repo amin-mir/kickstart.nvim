06fd7b43c0f746e19a9a4eea5c59ca6d541dc6aa
Amin Mir <aminmir326@gmail.com>
2024-03-23 13:46:51 +0200

    Migrate to harpoon v2

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,22 +1,33 @@
 return {
   'ThePrimeagen/harpoon',
-  version = "*",
+  branch = 'harpoon2',
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
   },
   config = function()
-    require('harpoon').setup {}
+    local harpoon = require 'harpoon'
+    harpoon:setup()
 
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    vim.keymap.set('n', '<leader>ha', function()
+      harpoon:list():append()
+    end, { desc = 'Harpoon toggle mark file' })
 
-    vim.keymap.set("n", "<leader>ha", mark.toggle_file, { desc = 'Toggle mark file for harpoon' })
-    vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu, { desc = 'Show marked files for harpoon' })
+    vim.keymap.set('n', '<leader>hh', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon show marked files' })
 
-    vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end, { desc = 'Harpoon file #1' })
-    vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end, { desc = 'Harpoon file #2' })
-    vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end, { desc = 'Harpoon file #3' })
-    vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end, { desc = 'Harpoon file #4' })
+    vim.keymap.set('n', '<leader>h1', function()
+      harpoon:list():select(1)
+    end, { desc = 'Harpoon file #1' })
+    vim.keymap.set('n', '<leader>h2', function()
+      harpoon:list():select(2)
+    end, { desc = 'Harpoon file #2' })
+    vim.keymap.set('n', '<leader>h3', function()
+      harpoon:list():select(3)
+    end, { desc = 'Harpoon file #3' })
+    vim.keymap.set('n', '<leader>h4', function()
+      harpoon:list():select(3)
+    end, { desc = 'Harpoon file #4' })
 
     -- To manually run harpoon commands, use the following format:
     -- :lua require("harpoon.ui").nav_file(1)

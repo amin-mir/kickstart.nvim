1af56dc1f15718ab4a8bbc837a342ddc79e9f7e2
Amin Mir <aminmir326@gmail.com>
2024-03-28 10:22:22 +0200

    Ignore go vendor files in Telescope search

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,5 @@
 -- NeoTree shortcuts
-vim.keymap.set('n', '<leader>ff', '<Cmd>Neotree position=float reveal toggle<CR>',
-  { desc = 'Open Neotree in floating mode' })
+vim.keymap.set('n', '<leader>ff', '<Cmd>Neotree position=float reveal toggle<CR>', { desc = 'Open Neotree in floating mode' })
 vim.keymap.set('n', '<leader>fb', '<Cmd>Neotree source=buffers position=float toggle<CR>', { desc = 'Show open buffers' })
 
 -- Git
@@ -10,19 +9,17 @@ vim.keymap.set('n', '<leader>gb', '<Cmd>:GBrows<CR>', { desc = 'Brows to the cur
 function ToggleGoTestVerbose()
   if vim.g['test#go#gotest#options'] == '-v' then
     vim.g['test#go#gotest#options'] = ''
-    print("Verbose mode off for Go tests")
+    print 'Verbose mode off for Go tests'
   else
     vim.g['test#go#gotest#options'] = '-v'
-    print("Verbose mode on for Go tests")
+    print 'Verbose mode on for Go tests'
   end
 end
 
-vim.api.nvim_set_keymap('n', '<leader>cgv', ':lua ToggleGoTestVerbose()<CR>',
-  { noremap = true, silent = true, desc = 'Toggle [G]o Tests [V]erbose Mode' })
+vim.api.nvim_set_keymap('n', '<leader>cgv', ':lua ToggleGoTestVerbose()<CR>', { noremap = true, silent = true, desc = 'Toggle [G]o Tests [V]erbose Mode' })
 
 -- Copilot
-vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")',
-  { noremap = true, silent = true, expr = true, desc = 'Accept copilot suggestion' })
+vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true, desc = 'Accept copilot suggestion' })
 
 -- Switch between buffers
 -- vim.keymap.set('n', '<M-h>', '<C-w>h', { noremap = true, silent = true, desc = 'Switch to left buffer' })
@@ -31,17 +28,13 @@ vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")',
 -- vim.keymap.set('n', '<M-l>', '<C-w>l', { noremap = true, silent = true, desc = 'Switch to right buffer' })
 vim.g.tmux_navigator_no_mappings = 1
 
-vim.keymap.set('n', '<M-h>', ":TmuxNavigateLeft<cr>",
-  { noremap = true, silent = true, desc = 'Switch to left buffer' })
-vim.keymap.set('n', '<M-k>', ":TmuxNavigateUp<cr>",
-  { noremap = true, silent = true, desc = 'Switch to top buffer' })
-vim.keymap.set('n', '<M-j>', ":TmuxNavigateDown<cr>",
-  { noremap = true, silent = true, desc = 'Switch to bottom buffer' })
-vim.keymap.set('n', '<M-l>', ":TmuxNavigateRight<cr>",
-  { noremap = true, silent = true, desc = 'Switch to right buffer' })
+vim.keymap.set('n', '<M-h>', ':TmuxNavigateLeft<cr>', { noremap = true, silent = true, desc = 'Switch to left buffer' })
+vim.keymap.set('n', '<M-k>', ':TmuxNavigateUp<cr>', { noremap = true, silent = true, desc = 'Switch to top buffer' })
+vim.keymap.set('n', '<M-j>', ':TmuxNavigateDown<cr>', { noremap = true, silent = true, desc = 'Switch to bottom buffer' })
+vim.keymap.set('n', '<M-l>', ':TmuxNavigateRight<cr>', { noremap = true, silent = true, desc = 'Switch to right buffer' })
 
 -- Define the DiagnosticToggle command
-vim.api.nvim_create_user_command("DiagnosticToggle", function()
+vim.api.nvim_create_user_command('DiagnosticToggle', function()
   local config = vim.diagnostic.config
   local current_config = config()
   config {
@@ -49,8 +42,7 @@ vim.api.nvim_create_user_command("DiagnosticToggle", function()
     underline = not current_config.virtual_text,
     signs = not current_config.virtual_text,
   }
-end, { desc = "Toggle Diagnostics" })
+end, { desc = 'Toggle Diagnostics' })
 
 -- Create a keymap for the DiagnosticToggle command
-vim.api.nvim_set_keymap('n', '<leader>cd', '<cmd>DiagnosticToggle<CR>',
-  { noremap = true, silent = true, desc = "Toggle Diagnostics" })
+vim.api.nvim_set_keymap('n', '<leader>cd', '<cmd>DiagnosticToggle<CR>', { noremap = true, silent = true, desc = 'Toggle Diagnostics' })

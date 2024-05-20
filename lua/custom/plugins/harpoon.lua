fee073af735c183590bf814d5f6869bbc8b7880e
Amin Mir <aminmir326@gmail.com>
2024-05-20 14:55:56 +0300

    Use add instead of append

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     harpoon:setup()
 
     vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end, { desc = 'Harpoon toggle mark file' })
 
     vim.keymap.set('n', '<leader>hh', function()
